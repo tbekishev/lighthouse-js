57d0a764e7f9856bf90667561c1a5ab9d57fefc5
Tuigun Bekishev <tbekishev@gmail.com>
2022-09-18 17:16:59 -0300

    updated loopyLighthouse

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,15 +1,20 @@
-for ( let i = 100; i < 201; i++) {
+function loopyLighthouse(range, multiples, words) {
+  
+  
+  for ( let i = range[0]; i <= range[1]; i++) {
 
-	if (i % 3 ===0 && i % 4 ===0) {
-	console.log('LoopyLighthouse');
-}
-	else if (i % 3===0) {
-	console.log('Loopy');
-}
-	else if (i % 4 ===0) {
-	console.log('Lighthouse');
-}	else {
-	console.log(i);
-}
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    }
+    else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    }
+    else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
 
+  }
 }
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
